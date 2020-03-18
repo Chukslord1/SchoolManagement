5f0507406745c4f8ebe0cd572a22e08e865693aa
Chukslord1 <chukslord1@gmail.com>
2020-03-18 06:21:03 -0700

    finished login function

--- App/models.py
+++ App/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
-    user_type = (
+    USER_TYPE_CHOICES = (
         ('Parent', 'Parent'),
         ('Student', 'Student'),
         ('Teacher', 'Teacher'),
@@ -13,6 +13,7 @@ class UserProfile(models.Model):
         ('Liberian', 'Liberian'),
 
     )
+    user_type = models.CharField(max_length=100, choices=USER_TYPE_CHOICES)
     secret_pin = models.IntegerField()
 
     def __str__(self):
