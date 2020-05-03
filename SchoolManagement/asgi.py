0ae113c4485776b273aab297935659b6c73943ca
Chukslord1 <chukslord1@gmail.com>
2020-05-03 09:38:15 +0500

    int rec websock

--- SchoolManagement/asgi.py
+++ SchoolManagement/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+
 from django.core.asgi import get_asgi_application
+from SchoolManagement.websocket import websocket_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SchoolManagement.settings')
 
 application = get_asgi_application()
+async def application(scope, receive, send):
+    if scope['type'] == 'http':
+        await application(scope, receive, send)
+    elif scope['type'] == 'websocket':
+        await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
